e1ebce42cfb3bb7aadc79356cd984241da88efe8
Luis <luisf3rnando29@outlook.com>
2025-03-12 18:28:24 -0300

    Add presentation frame to HeroSection with animated background and description

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,19 @@
-import './HeroSection.css';
+import "./HeroSection.css";
 
-export default function HeroSection() {   
-    return ( 
-        <div className="hero-container">
-            <h1 className='name'>Luis Fernando</h1>
-        </div>
-    )
+export default function HeroSection() {
+  return (
+    <div className="hero-container">
+      <h1 className="name">Luis Fernando</h1>
+      <div className="presentation-frame">
+        <h3>
+          Olá, sou Luis Fernando dos Santos, tenho 19 anos e sou um entusiasta
+          em desenvolvimento de sistemas. Adoro desenvolver soluções
+          criativas e estou constantemente procurando por novas aprendizagens em
+          tecnologias que possam melhorar minhas competências. Meu foco
+          principal é o desenvolvimento web, mas estou sempre aberto a novas
+          oportunidades e desafios.
+        </h3>
+      </div>
+    </div>
+  );
 }
