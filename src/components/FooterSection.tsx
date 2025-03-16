79eeaf73cd4841fb029cf7ed68939817c9b7757f
Luis <luisf3rnando29@outlook.com>
2025-03-16 12:41:34 -0300

    Add FooterSection component with styling and integrate it into Home page

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -1,8 +1,22 @@
+import "./FooterSection.css";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; {new Date().getFullYear()} Luis Fernando dos Santos. All rights reserved.</p>
+      <div className="footer-icons">
+        <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">
+        </a>
+        <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
+          
+        </a>
+        <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer">
+         
+        </a>
+      </div>
+    </footer>
+  );
+};
 
-export default function FooterSection() {
-    <h1>
-        Teste
-    </h1>
-
-}
+export default Footer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "../components/HeroSection";
 import SkillSection from "../components/SkillsSection";
+import FooterSection from "../components/FooterSection"
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
         <HeroSection />
         <SkillSection />
+        <FooterSection />
     </div>
   );
 }
