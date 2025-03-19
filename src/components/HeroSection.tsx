ae988e1f835f69baa24b422af5c17efe83bb7937
Luis <luisf3rnando29@outlook.com>
2025-03-19 00:01:30 -0300

    Refactor HeroSection layout and enhance typing effect with additional text

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,55 +3,77 @@ import "./HeroSection.css";
 
 export default function HeroSection() {
   const text = "Olá meu nome é ";
-  const [index, setIndex] = useState(0);
-  const [textAnimated, setTextAnimated] = useState("");
-
-  function typeWriter() {
-    setIndex((prev) => prev + (1 % text.length));
-    setTextAnimated((prev) => prev + text.charAt(index));
-  }
+  const text2 = "Luis Fernando Dos Santos :)";
   
+  const [index, setIndex] = useState(0);
+  const [index2, setIndex2] = useState(0);
+  const [textAnimated, setTextAnimated] = useState("");
+  const [textAnimated2, setTextAnimated2] = useState("");
+
   useEffect(() => {
-    const interval = setInterval(typeWriter, 90);
-    return () => clearInterval(interval);
+    if (index < text.length) {
+      const interval = setInterval(() => {
+        setTextAnimated((prev) => prev + text.charAt(index));
+        setIndex((prev) => prev + 1);
+      }, 90);
+
+      return () => clearInterval(interval);
+    }
   }, [index]);
+
+  useEffect(() => {
+    if (index === text.length && index2 < text2.length) {
+      const interval2 = setInterval(() => {
+        setTextAnimated2((prev) => prev + text2.charAt(index2));
+        setIndex2((prev) => prev + 1);
+      }, 90);
+
+      return () => clearInterval(interval2);
+    }
+  }, [index2, index]);
 
   return (
     <div className="hero-container">
-      <h1 className="mynameis">{textAnimated} &nbsp; </h1>
-      <h1 className="name">Luis Fernando Dos Santos</h1>
-      <span className="devtext">Desenvolvedor de sistemas</span>
-      <div className="buttons-container">
-        <button className="button-cv">Dowload CV</button>
-        <button className="button-contate-me">Entre em contato</button>
+      <div className="cards-container">
+        <div className="name-presentaion">
+          <h1 className="mynameis">{textAnimated} &nbsp; </h1>
+          <h1 className="name">{textAnimated2}</h1>
+          <span className="devtext">Desenvolvedor de sistemas</span>
+          <div className="buttons-container">
+            <button className="botao">Dowload CV</button>
+            <button className="botao">Entre em contato</button>
+          </div>
+        </div>
+
+        <div className="settings-card">
+          <div className="card-vscode">
+            <i className="fab fa-vscode"></i>
+            <img className="img-vs" src="/assets/vscode.png" />
+            <p className="text-settings-card">VS</p>
+          </div>
+          <div className="card-vscode">
+            <i className="fab fa-vscode"></i>
+            <img className="img-vs" src="/assets/github.png" />
+            <p className="text-settings-card">GITHUB</p>
+          </div>
+          <div className="card-vscode">
+            <i className="fab fa-vscode"></i>
+            <img className="img-vs" src="/assets/spotify.png" />
+            <p className="text-settings-card">SPOTIFY</p>
+          </div>
+        </div>
       </div>
-      <div className="presentation-frame">
+
+      {/* <div className="presentation-frame"> 
         <h3>
           Tenho 19 anos e sou um entusiasta em desenvolvimento de sistemas.
           Adoro desenvolver soluções criativas e estou constantemente procurando
           por novas aprendizagens em tecnologias que possam melhorar minhas
           competências. Meu foco principal é o desenvolvimento web, mas estou
-          sempre aberto a novas oportunidades e desafios.
+          sempre aberto a novas oportunidades e desafios. 
         </h3>
       </div>
-
-      <div className="settings-card">
-        <div className="card-vscode">
-          <i className="fab fa-vscode"></i>
-          <img className="img-vs" src="/assets/vscode.png" />
-          <p className="text-settings-card">VS</p>
-        </div>
-        <div className="card-vscode">
-          <i className="fab fa-vscode"></i>
-          <img className="img-vs" src="/assets/github.png" />
-          <p className="text-settings-card">GITHUB</p>
-        </div>
-        <div className="card-vscode">
-          <i className="fab fa-vscode"></i>
-          <img className="img-vs" src="/assets/spotify.png" />
-          <p className="text-settings-card">SPOTIFY</p>
-        </div>
-      </div>
+      */}
     </div>
   );
 }
